60e50fc476cbe8091f6de17a70b99e622a82bdee
Ganesh <ganesh.gavhane@NLPULTP4603899.FPS.NIHILENT.com>
2024-01-12 14:58:37 +0530

    test

---
+++ src/interviews/ReverseStringWithoutChanginSeq.java
@@ -0,0 +1,26 @@
+package interviews;
+
+public class ReverseStringWithoutChanginSeq {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 String s = "i.like.this.program.very.much";
+         String st []= s.split("\\.");
+         int len = st.length;
+         String rev ="";
+         for(int i=len-1;i>=0;i--)
+         {
+             rev=rev+st[i];
+             System.out.println(rev);
+             
+             if(i!=0)
+             {
+            	 rev+=".";
+             }
+             
+         }
+         
+	}
+
+}
---
+++ src/interviews/SwapString.java
@@ -0,0 +1,27 @@
+package interviews;
+
+public class SwapString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a="abc";
+		
+		String b="hfhf";
+		
+		a=a+b;
+		
+		b=a.substring(0,a.length()-b.length());
+		
+		a=a.substring(b.length());
+		
+		System.out.println("value of a is:"+a);
+		System.out.println("value of a is:"+b);
+		
+		
+		
+		
+
+}
+
+	}
+
